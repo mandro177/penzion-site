53ae810d5cff7f4029e50da19b2e49b67590262d
mandro177 <mandro.mandro177gmail.com>
2017-03-16 23:22:04 +0100

    vytvorenie kontakt.php

---
+++ kontakt.php
@@ -0,0 +1,31 @@
+<?php include_once '_include/header.php'; ?>
+
+<div class="container">
+<div class="col-md-5">
+    <div class="form-area">  
+        
+        <br>
+                    <h3 style="margin-bottom: 25px; text-align: center;">Kontaktujte nás</h3>
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="Meno">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="Email" >
+                    </div>
+                    
+                    <div class="form-group">
+                        <input type="text" class="form-control"  placeholder="Predmet">
+                    </div>
+                    <div class="form-group">
+
+                    <textarea class="form-control" type="textarea"  placeholder="Vaša správa"  rows="4"></textarea>
+                                            
+                    </div>
+            
+        <button type="button" class="btn-default pull-right sendmessage" >Odošli správu</button>
+        
+    </div>
+</div>
+</div>
+
+<?php include_once '_include/footer.php'; ?>
