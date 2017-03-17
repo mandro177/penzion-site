6df6fdc8772f690c31bbc1aa2b2a44d2be17fa85
mandro177 <mandro.mandro177gmail.com>
2017-03-17 11:50:08 +0100

    update_kontakt.php

--- kontakt.php
+++ kontakt.php
@@ -1,31 +1,96 @@
 <?php include_once '_include/header.php'; ?>
 
 <div class="container">
-<div class="col-md-5">
+
+<div class="col-md-5 col-lg-5">
+    <div class="adresa">  
+        
+        <br>
+        <h3 style="margin-bottom: 25px; text-align: left;">Kontaktujte nás</h3>
+                    
+        <ul>
+                           
+                           <li>Penzión</li>
+                           <li>Ulica 267</li>
+                           <li>93529, Horná dolná</li>
+                           <li>Slovenská Republika</li>
+                            <br>
+                            
+                           <table class="table" >
+                    <tbody>
+                      <tr>
+                        <td class="col-md-5"><i class="fa fa-phone" aria-hidden="true"></i> Telefónne číslo:</td>
+                        <td class="stats">+421 912 654 125</td>
+                      </tr>
+                      
+                      <tr>
+                        <td><i class="fa fa-envelope" aria-hidden="true"></i> Email:</td>
+                        <td class="stats">info@penzion.sk </td>
+                      </tr>
+                      <tr>
+                        <td><i class="fa fa-globe" aria-hidden="true"></i> Web:</td>
+                        <td class="stats"><a href="#">www.penzion.sk</a></td>
+                      </tr>
+                   
+                         
+                             
+                        
+                     
+                    </tbody>
+                  </table>
+                       </ul>  
+    </div>
+
+
+</div>
+<div class="col-md-5 col-lg-5 col-lg-offset-1 col-md-offser-1 " style="margin-bottom:40px">
     <div class="form-area">  
         
         <br>
-                    <h3 style="margin-bottom: 25px; text-align: center;">Kontaktujte nás</h3>
+        <h3 style="margin-bottom: 25px; text-align:left">Napíšte nám</h3>
                     <div class="form-group">
-                        <input type="text" class="form-control"  placeholder="Meno">
+                       <input class="contact-input" placeholder="Meno" required id="meno" name="Meno" />
                     </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control"  placeholder="Email" >
+                   <div class="form-group">
+                       <input class="contact-input" placeholder="Email" required id="email" name="Email" />
                     </div>
                     
                     <div class="form-group">
-                        <input type="text" class="form-control"  placeholder="Predmet">
+                       <input class="contact-input" placeholder="Predmet" required id="meno" name="Predmet" />
                     </div>
                     <div class="form-group">
 
-                    <textarea class="form-control" type="textarea"  placeholder="Vaša správa"  rows="4"></textarea>
+                    <textarea class="contact-textarea" type="textarea"  placeholder="Vaša správa"  rows="4"></textarea>
                                             
                     </div>
             
         <button type="button" class="btn-default pull-right sendmessage" >Odošli správu</button>
         
     </div>
+
+
 </div>
+<br>
+
+<div class="gallery">
+            <div class="gallery-photo col-lg-3 col-md-3 col-sm-3 col-xs-6 ">
+                <img src="images/slide/contact_foto_1.jpg" class="img-responsive">
+            </div>
+            <div class="gallery-photo col-lg-3 col-md-3 col-sm-3 col-xs-6 ">
+                  <img src="images/slide/contact_foto_2.jpg" class="img-responsive">
+            </div>
+            <div class="gallery-photo col-lg-3 col-md-3 col-sm-3 col-xs-6 ">
+                <img src="images/slide/contact_foto_3.jpg" class="img-responsive">
+            </div>
+            <div class="gallery-photo col-lg-3 col-md-3 col-sm-3 col-xs-6 ">
+                  <img src="images/slide/contact_foto_4.jpg" class="img-responsive">
+            </div>
+
+            
+            </div>
+
+
+
 </div>
 
 <?php include_once '_include/footer.php'; ?>
