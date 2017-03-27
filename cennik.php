8222a15fd06c9fffbd1b2f3baab0f918ea9a5db8
mandro177 <mandro.mandro177gmail.com>
2017-03-27 17:36:51 +0200

    update cennik.php, app.css

--- cennik.php
+++ cennik.php
@@ -4,16 +4,16 @@
 	<br>
 		<div class="col-md-6 col-sm-6">
     		<span class="thumbnail">
-      			<img src="http://placehold.it/500x400" alt="...">
-      			<h3>Apartmán 1</h3>
+      			<img src="images/slide/izba1.jpg" alt="">
+      			<h3 class="h3-cena">Apartmán 1</h3>
       			
-      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<p class="p-cena">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       			<hr class="line">
-      			<div class="row">
+      			<div class="row cena-row">
       				<div class="col-md-6 col-sm-6">
       					<p class="cena"><strong>€70,90/</strong><small>noc</small></p>
       				</div>
-      				<div class="col-md-6 col-sm-6">
+      				<div class="col-md-6 col-sm-6" style="margin-top:10px">
       					<button class="btn btn-default pull-right sendmessage" > Detail</button>
       				</div>
       				
@@ -22,16 +22,17 @@
   		</div>
   		<div class="col-md-6 col-sm-6">
     		<span class="thumbnail">
-      			<img src="http://placehold.it/500x400" alt="...">
-      			<h3>Apartmán 2</h3>
+      			<img src="images/slide/izba2.jpg" alt="">
+
+      			<h3 class="h3-cena">Apartmán 2</h3>
       			
-      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<p class="p-cena">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       			<hr class="line">
-      			<div class="row">
+      			<div class="row cena-row">
       				<div class="col-md-6 col-sm-6">
       					<p class="cena"><strong>€62,90/</strong><small>noc</small></p>
       				</div>
-      				<div class="col-md-6 col-sm-6">
+      				<div class="col-md-6 col-sm-6" style="margin-top:10px">
       					<button class="btn btn-default pull-right sendmessage" > Detail</button>
       				</div>
       				
@@ -40,16 +41,17 @@
   		</div>
   		<div class="col-md-4 col-sm-6">
     		<span class="thumbnail">
-      			<img src="http://placehold.it/500x400" alt="...">
-      			<h3>Apartmán 1</h3>
+      			<img src="images/slide/izba3.jpg" alt="">
+
+      			<h3 class="h3-cena">Jednolôžková izba 1</h3>
       			
-      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<p class="p-cena">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       			<hr class="line">
-      			<div class="row">
+      			<div class="row cena-row">
       				<div class="col-md-6 col-sm-6">
       					<p class="cena"><strong>€13,90/</strong><small>noc</small></p>
       				</div>
-      				<div class="col-md-6 col-sm-6">
+      				<div class="col-md-6 col-sm-6" style="margin-top:10px">
       					<button class="btn btn-default pull-right sendmessage" > Detail</button>
       				</div>
       				
@@ -57,16 +59,17 @@
     		</span>
   		</div><div class="col-md-4 col-sm-6">
     		<span class="thumbnail">
-      			<img src="http://placehold.it/500x400" alt="...">
-      			<h3>Apartmán 1</h3>
+      			<img src="images/slide/izba4.jpg" alt="">
+
+      			<h3 class="h3-cena">Dvojlôžková izba</h3>
       			
-      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<p class="p-cena">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       			<hr class="line">
-      			<div class="row">
+      			<div class="row cena-row" >
       				<div class="col-md-6 col-sm-6">
       					<p class="cena"><strong>€17,90/</strong><small>noc</small></p>
       				</div>
-      				<div class="col-md-6 col-sm-6">
+      				<div class="col-md-6 col-sm-6" style="margin-top:10px">
       					<button class="btn btn-default pull-right sendmessage" > Detail</button>
       				</div>
       				
@@ -74,16 +77,17 @@
     		</span>
   		</div><div class="col-md-4 col-sm-6">
     		<span class="thumbnail">
-      			<img src="http://placehold.it/500x400" alt="...">
-      			<h3>Apartmán 1</h3>
+      			<img src="images/slide/izba5.jpg" alt="">
+
+      			<h3 class="h3-cena">Trojlôžková izba</h3>
       			
-      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<p class="p-cena">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       			<hr class="line">
-      			<div class="row">
+      			<div class="row cena-row">
       				<div class="col-md-6 col-sm-6">
       					<p class="cena"><strong>€20,90/</strong><small>noc</small></p>
       				</div>
-      				<div class="col-md-6 col-sm-6">
+      				<div class="col-md-6 col-sm-6" style="margin-top:10px">
       					<button class="btn btn-default pull-right sendmessage" > Detail</button>
       				</div>
       				
