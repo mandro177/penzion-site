f3439e5b36b3492ed398f17a39d96f19be076d1f
mandro177 <mandro.mandro177gmail.com>
2017-03-27 16:52:04 +0200

    update cennik.php

--- cennik.php
+++ cennik.php
@@ -1,3 +1,96 @@
 <?php include_once '_include/header.php'; ?>
-
+<div class="container">
+	<div class="row">
+	<br>
+		<div class="col-md-6 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="http://placehold.it/500x400" alt="...">
+      			<h3>Apartmán 1</h3>
+      			
+      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<hr class="line">
+      			<div class="row">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="cena"><strong>€70,90/</strong><small>noc</small></p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      					<button class="btn btn-default pull-right sendmessage" > Detail</button>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		<div class="col-md-6 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="http://placehold.it/500x400" alt="...">
+      			<h3>Apartmán 2</h3>
+      			
+      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<hr class="line">
+      			<div class="row">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="cena"><strong>€62,90/</strong><small>noc</small></p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      					<button class="btn btn-default pull-right sendmessage" > Detail</button>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		<div class="col-md-4 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="http://placehold.it/500x400" alt="...">
+      			<h3>Apartmán 1</h3>
+      			
+      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<hr class="line">
+      			<div class="row">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="cena"><strong>€13,90/</strong><small>noc</small></p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      					<button class="btn btn-default pull-right sendmessage" > Detail</button>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div><div class="col-md-4 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="http://placehold.it/500x400" alt="...">
+      			<h3>Apartmán 1</h3>
+      			
+      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<hr class="line">
+      			<div class="row">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="cena"><strong>€17,90/</strong><small>noc</small></p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      					<button class="btn btn-default pull-right sendmessage" > Detail</button>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div><div class="col-md-4 col-sm-6">
+    		<span class="thumbnail">
+      			<img src="http://placehold.it/500x400" alt="...">
+      			<h3>Apartmán 1</h3>
+      			
+      			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+      			<hr class="line">
+      			<div class="row">
+      				<div class="col-md-6 col-sm-6">
+      					<p class="cena"><strong>€20,90/</strong><small>noc</small></p>
+      				</div>
+      				<div class="col-md-6 col-sm-6">
+      					<button class="btn btn-default pull-right sendmessage" > Detail</button>
+      				</div>
+      				
+      			</div>
+    		</span>
+  		</div>
+  		
+	</div>
+</div>
 <?php include_once '_include/footer.php'; ?>
